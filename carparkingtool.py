6bb6b381fbf2da142750a68889b66f92f96f8d5f
CPMFlankerTool <eyodamao@gmail.com>
2025-04-19 03:29:22 +0500

    Update and rename mdserge.py to carparkingtool.py

--- carparkingtool.py
+++ carparkingtool.py
@@ -1,8 +1,8 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://mdserge.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://carparkingtool.squareweb.app/api"
 
-class MdSerge:
+class CarParkingTool:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
